81875cc0194c53ff42776a59b7eb410925359a3a
Ibrahim Mammadov <zxyctn@gmail.com>
2021-08-15 22:01:28 +0200

    Add new model

--- test.m
+++ test.m
@@ -1,5 +1,6 @@
-img = imread("data/fork/20210805_080920.jpg");
+load('model/TrainedAlexNetModel.mat')
 
+img = imread("data/fork/20210805_080920.jpg");
 img = imresize(img, [227, 227]);
 
 y = classify(trainedNetwork_1, img)
