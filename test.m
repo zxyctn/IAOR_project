0f207a05dbfdd4cb5f0a76b53ce50f2eedd5715a
zxyctn <zxyctn@gmail.com>
2021-08-17 16:55:57 +0200

    Add image acquisition files, final model

--- test.m
+++ test.m
@@ -1,6 +1,32 @@
-load('model/TrainedAlexNetModel.mat')
+%'fork'
+[t, f] = get_results('fork')
 
-img = imread("data/fork/20210805_080920.jpg");
-img = imresize(img, [227, 227]);
+%'butterknife'
+[t, f] = get_results('butterknife')
 
-y = classify(trainedNetwork_1, img)
+%'teaspoon'
+[t, f] = get_results('teaspoon')
+
+function [t, f] = get_results(category)
+    load('model/TrainedAlexNetModel5.mat')
+    % Get all the data for that specified category
+    imgs = dir(strcat('testing/', strcat(category, '/*.jpg')));      
+    % Get the number of images in that category
+    n = length(imgs)
+    % Initialization of variables for correct and incorrect classification
+    t = 0;
+    f = 0;
+
+    for i = 1:n
+       %img_name = imgs(randi([1, n], 1, 1)).name;
+       img_name = imgs(i).name;
+       img = imread(strcat(strcat('testing/', strcat(category, '/'), img_name)));
+       img = imresize(img, [227, 227]);
+       y = classify(trainedNetwork_3, img);
+       if (y == category)
+           t = t + 1;
+       else
+           f = f + 1;
+       end
+    end
+end
